9b5101b78b8bbafcefbb66482b92469dc1633f4f
anaisrabary <anais.rabary@gmail.com>
2018-11-27 12:15:41 +0100

    change Anaîs Graphs and add forest data

--- analyseAnais.R
+++ analyseAnais.R
@@ -2,17 +2,21 @@ library(ggplot2)
 library(plyr)
 library(stringr)
 library(dplyr)
+library(tidyr)
 
 source(file="preprocessing.R")
+#source(file="preprocessing_tibble.R")
 str(olympic_dataset)
 
 #keep only data from 1980 till now (10 olympic games)
 olympic_dataset = olympic_dataset[olympic_dataset$'Year' >= 1980,]
+olympic_dataset = left_join(olympic_dataset, noc_dataset, by='NOC')
+olympic_dataset['Year'] <- lapply(olympic_dataset['Year'], factor)
 
 #************************************************************************
 # GOAL : Count medal each country got
 medal_per_country <- ddply(olympic_dataset, 
-                           .(NOC), 
+                           .(region), 
                            function(x){
                              gold<- sum(x$Medal=="Gold")
                              silver<- sum(x$Medal =="Silver")
@@ -21,25 +25,35 @@ medal_per_country <- ddply(olympic_dataset,
                              }  )
 
 
-#filter medals dols != 0
+#filter medals godl != 0
 medal_per_country <- medal_per_country[medal_per_country$Gold!=0,]
+top_gold = top_n(arrange(medal_per_country,desc(Gold)), 20, Gold)
 
 #display NOC with gold medals over the past 10 olympics games
-ggplot(data=medal_per_country, aes(NOC,Gold))+geom_col()
+t <- ggplot(data=top_gold, aes(reorder(region, -Gold), Gold)) + geom_col(aes(fill=region)) + guides(fill="none")
+t+labs(x="Regions", y="Gold medals", title = "Top 20 regions that won Gold medals in the past 10 Olympics Games ")
+#x= reorder(NOC,-Gold), y = Gold)
+#ggplot(top_medal, aes(x = reorder(NOC, -Total), y = Total)) + geom_bar(stat="identity", aes(fill=NOC)) + coord_flip()
+
 
 #*************************************************************
 
 # GOALS : see if the height of athletes has an influence of winning medals
 # ranger par ordre décroissant de taille les athletes
-medals_per_height <- ddply(filter(olympic_dataset, olympic_dataset$Height!="NA"),
-                           .(Height),
+olympic_dataset <- drop_na(olympic_dataset, Height)
+medals_per_height <- ddply(olympic_dataset,
+                           .(Height, Sex),
                            function(x){
                              medals <- sum(x$Medal=="Gold" | x$Medal=="Sylver" | x$Medal =="Bronze")
                              data.frame(Medals= medals)
                            }  )
 
 # display medals depending of height.                         
-ggplot(data=medals_per_height,aes(Height, Medals))+geom_col()
+t<- ggplot(data=medals_per_height,aes(Height, Medals))+geom_col(position="dodge", aes(fill=Sex))
+#+geom_line(data = medals_per_height,aes(x=Height, y=Medals))
+t+labs(x="Height", y="Total Medals (Gold, Silver, Bronze)", title = "Total number of medals won by Athletes depending on their Height", subtitle="over the past 10 Olympic Games")
+
+
 # it is gaussian. 
 
 #*****************************************************************
@@ -47,14 +61,19 @@ ggplot(data=medals_per_height,aes(Height, Medals))+geom_col()
 
 # GOALS : see if the height of athletes has an influence of winning medals
 # ranger par ordre décroissant de taille les athletes
-medals_per_height_year <- ddply(filter(olympic_dataset, olympic_dataset$Height!="NA"),
-                           .(Height, Year),
+#filter(olympic_dataset,Height>=160 & Height <=190)
+medals_per_height_year <- ddply(filter(olympic_dataset,Height>=160 & Height <=200),
+                           .(Height, Year, Sex),
                            function(x){
                              medals <- sum(x$Medal=="Gold" | x$Medal=="Sylver" | x$Medal =="Bronze")
                              data.frame(Medals= medals)
                            }  )
+str(medals_per_height_year)
 
 # display medals depending of height and year.                         
-ggplot(data=medals_per_height_year,aes(Height, Medals))+geom_col(aes(fill=Year))
+ggplot(data=medals_per_height_year,aes(Height, Medals, group=Year))+geom_col(position="dodge",aes(fill=Year))
+#+stat_ydensity()
+#+geom_boxplot()
+#ggplot(data=medals_mean_height,aes(mean_height, Medals, group=Year))+geom_col(position="dodge",aes(fill=Year))
 
 
--- preprocessing.R
+++ preprocessing.R
@@ -1,6 +1,6 @@
 ### Data Preprocessing 
 
-### Maybe use full in future:
+### Maybe useful in the future:
 ## === turn 0,1 into boolean for multiple columns:
 # index_boolean = c("algra", "alsuc", "diete", "enric", "rien") # index column that indice boolean in tab_conso
 # tab_consommation[, index_boolean] = tab_consommation[, index_boolean] == 1
@@ -11,12 +11,16 @@
 # load dataset
 olympic_dataset = read.csv2("data/olympic-history-athletes-and-results/athlete_events.csv",sep=",", dec=".")
 noc_dataset =     read.csv2("data/olympic-history-athletes-and-results/noc_regions.csv",sep=",", dec=".")
+forest_dataset = read.csv2("data/forest/API_AG.LND.FRST.ZS_DS2_en_csv_v2_10224801.csv",sep=",", dec=".")
 
 # For athletes got NA medal, will receive "None" medal
 levels                      <- levels(olympic_dataset$Medal)
 levels[length(levels) + 1]  <- "None"                                     # Get levels and add "None"
 olympic_dataset$Medal <- factor(olympic_dataset$Medal, levels = levels)   # refactor include "None" as a factor level
-# Not need, it will complicate the situation
+# Not needed, it will complicate the situation
 olympic_dataset$Medal[is.na(olympic_dataset$Medal)] <- "None"             # replace NA with "None"
 
+# Transform to factor
+factor_cols <- c('Sex', 'NOC', 'Medal', 'Sport')
+olympic_dataset[factor_cols] <- lapply(olympic_dataset[factor_cols], factor)
 
