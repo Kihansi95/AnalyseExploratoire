a21ec3baa90cae2d66c819f8170558807daf2fa8
anaisrabary <anais.rabary@gmail.com>
2018-12-05 21:11:09 +0100

    taille homme femme

--- preprocessing.R
+++ preprocessing.R
@@ -12,14 +12,6 @@
 olympic_dataset = read.csv2("data/olympic-history-athletes-and-results/athlete_events.csv",sep=",", dec=".")
 noc_dataset =     read.csv2("data/olympic-history-athletes-and-results/noc_regions.csv",sep=",", dec=".")
 
-#PIBMetadata_dataset = read.csv2("data/PIB-grossDomesticProduct_1980-2017/WID_Metadata_30112018-130904.csv", sep=",", dec=".")
-PIB_dataset = read.csv2("data/PIB-grossDomesticProduct_1980-2017/WID_Data_30112018-130904.csv", 
-                        header = FALSE,col.names =c("region", "Indicator", "Percentile", "Year", "PIB"),sep=";", dec=".")
-PIB_datasetFULL = read.csv2("data/PIB-grossDomesticProduct_1896-2017/WID_Data_30112018-144413.csv", 
-                        header = FALSE,col.names =c("region", "Indicator", "Percentile", "Year", "PIB"),sep=";", dec=".")
-# delete row that have the value "null" for country :
-#PIB_dataset = PIB_dataset[-which(PIB_dataset$region =="null"), ]
-
 # For athletes got NA medal, will receive "None" medal
 levels                      <- levels(olympic_dataset$Medal)
 levels[length(levels) + 1]  <- "None"                                     # Get levels and add "None"
@@ -30,4 +22,16 @@ olympic_dataset$Medal[is.na(olympic_dataset$Medal)] <- "None"             # repl
 # Transform to factor
 factor_cols <- c('Sex', 'NOC', 'Medal', 'Sport')
 olympic_dataset[factor_cols] <- lapply(olympic_dataset[factor_cols], factor)
+
+
+
+
+#PIBMetadata_dataset = read.csv2("data/PIB-grossDomesticProduct_1980-2017/WID_Metadata_30112018-130904.csv", sep=",", dec=".")
+#PIB_dataset = read.csv2("data/PIB-grossDomesticProduct_1980-2017/WID_Data_30112018-130904.csv", 
+                        #header = FALSE,col.names =c("region", "Indicator", "Percentile", "Year", "PIB"),sep=";", dec=".")
+PIB_dataset= read.csv2("data/PIB-grossDomesticProduct_1896-2017/WID_Data_30112018-144413.csv", 
+                        header = FALSE,col.names =c("region", "Indicator", "Percentile", "Year", "PIB"),sep=";", dec=".")
+# delete row that have the value "null" for country :
+#PIB_dataset = PIB_dataset[-which(PIB_dataset$region =="null"), ]
+
 
